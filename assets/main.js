c51301b692d903d45ad1fe94699084c66404d94f
rizqicrows <rizqi.crows2@gmail.com>
2024-07-02 19:18:22 +0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,54 @@
+function cekKhodam(event) {
+    event.preventDefault();
+    let nama = $('#search').val();
+    if (nama === '') {
+        alert('Masukkan nama dulu blog!');
+    } else {
+        let status = [
+            'isi',
+            'kosong',
+        ]
+        const randomStatus = Math.floor(Math.random() * status.length);
+        if (randomStatus === 0) {
+            let khodam = [
+                'Tobrut',
+                'Sempol Ayam',
+                'Teh Botol',
+                'Mimi Peri',
+                'Masako Ayam',
+                'Tahu Bulat',
+                'Jamal',
+                'Risol Mayo',
+                'Dora',
+                'Tomket',
+                'Asep',
+                'Ragil',
+                'Monyet Tantrum',
+                'Rawageni',
+                'Joker',
+                'Kecubung',
+                'Pertalite',
+                'Boncabe',
+                'Stella Jeruk',
+                'Mio Mirza',
+                'Adem Sari',
+                'ES Kelapa Sawit',
+                'Panjang x Lebar',
+                'Sengkuni',
+                'Naga Imut',
+                'Tuna Mayo'
+            ];
+            const random = Math.floor(Math.random() * khodam.length);
+            $('#result').html(`${nama.toUpperCase()} - Khodam Kamu (${khodam[random]})`);
+            $('#search').val('');
+        }
+        else {
+            $('#result').html(`${nama.toUpperCase()} - KOSONG`);
+            $('#search').val('');
+        }
+    }
+}
+
+$(document).ready(function () {
+    $('#btn-search').click(cekKhodam);
+});
